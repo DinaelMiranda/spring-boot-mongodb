834fb273910724b9ca0372085b8e144602cdcce4
dinael <tadsdinael@gmail.com>
2021-03-28 13:30:30 -0300

    Inserção de Usuário com POST

--- src/main/java/com/dithec/workshopmongo/services/UserService.java
+++ src/main/java/com/dithec/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.dithec.workshopmongo.services;
 
 import com.dithec.workshopmongo.domain.User;
+import com.dithec.workshopmongo.dto.UserDTO;
 import com.dithec.workshopmongo.repository.UserRepository;
 import com.dithec.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +21,17 @@ public class UserService {
         return repo.findAll();
     }
 
-    public User findById(String id) {
-        Optional<User> obj = repo.findById(id);
-        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+        public User findById(String id) {
+            Optional<User> obj = repo.findById(id);
+            return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+        }
+
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
