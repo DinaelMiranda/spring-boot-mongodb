97b3ec283e71259ef756a8c640646f3894eb4073
dinael <tadsdinael@gmail.com>
2021-03-28 16:33:12 -0300

    Consulta Simples com queryMethods

--- src/main/java/com/dithec/workshopmongo/resources/PostResource.java
+++ src/main/java/com/dithec/workshopmongo/resources/PostResource.java
@@ -4,6 +4,7 @@ package com.dithec.workshopmongo.resources;
 import com.dithec.workshopmongo.domain.Post;
 import com.dithec.workshopmongo.domain.User;
 import com.dithec.workshopmongo.dto.UserDTO;
+import com.dithec.workshopmongo.resources.utils.URL;
 import com.dithec.workshopmongo.services.PostService;
 import com.dithec.workshopmongo.services.UserService;
 import javafx.geometry.Pos;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.net.URLDecoder;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,6 +31,15 @@ public class PostResource {
 
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+
+    }
+
+    @GetMapping("/titlesearch")
+    public ResponseEntity<List<Post>> findByIdTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+
+        return ResponseEntity.ok().body(list);
 
     }
 }
